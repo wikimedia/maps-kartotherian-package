eec0b38a112575d6e0fcbd4c183656b7d376d423
Guillaume Lederrey <glederrey@wikimedia.org>
2017-11-29 11:07:11 +0100

    align package.json with currently deployed version
    Bug: T162241
    Change-Id: I2528c345802084163d384d7d57b4f083f2f82077

--- app.js
+++ app.js
@@ -1,47 +1,4 @@
 'use strict';
 
-var kartotherian = require('kartotherian')
-
-/**
- * Creates an express app and initialises it
- * @param {Object} options the options to initialise the app with
- * @return {bluebird} the promise resolving to the app object
- */
-function initApp(options) {
-    return kartotherian.app.initApp(options);
-}
-
-
-/**
- * Loads all routes declared in routes/ into the app
- * @param {Application} app the application object to load routes into
- * @returns {bluebird} a promise resolving to the app object
- */
-function loadRoutes (app) {
-    return kartotherian.app.loadRoutes(app);
-}
-
-
-/**
- * Creates and start the service's web server
- * @param {Application} app the app object to use in the service
- * @returns {bluebird} a promise creating the web server
- */
-function createServer(app) {
-    return kartotherian.app.createServer(app);
-}
-
-
-/**
- * The service's entry point. It takes over the configuration
- * options and the logger- and metrics-reporting objects from
- * service-runner and starts an HTTP server, attaching the application
- * object to it.
- */
-module.exports = function(options) {
-
-    return initApp(options)
-    .then(loadRoutes)
-    .then(createServer);
-
-};
+// just delegate everything to the main kartotherian package
+module.exports = require('kartotherian')
